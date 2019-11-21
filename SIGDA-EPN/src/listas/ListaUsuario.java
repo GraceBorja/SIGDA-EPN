635f921dc7b10e8e55bdb7c2ddc1871fc8d1e877
GraceBorja <gracegbm@hotmail.es>
2019-11-21 15:47:12 -0500

    ac
    ac

---
+++ SIGDA-EPN/src/listas/ListaUsuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listas;
+
+import java.util.ArrayList;
+import modelo.Usuario;
+
+/**
+ *
+ * @author Grace
+ */
+public class ListaUsuario extends ArrayList<Usuario>{
+    
+}
